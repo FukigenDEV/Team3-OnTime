e35fa756f26e8552a8ea7962443b16e777de1732
laREiGN <43932616+laREiGN@users.noreply.github.com>
2020-06-01 11:50:18 +0200

    Made the group adapter also open in a the fragment instead if just a view

--- PROTOTYPES/OnTimeApp/app/src/main/java/com/example/ontimeapp/SelectedGroup.java
+++ PROTOTYPES/OnTimeApp/app/src/main/java/com/example/ontimeapp/SelectedGroup.java
@@ -1,28 +1,70 @@
 package com.example.ontimeapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class SelectedGroup extends AppCompatActivity {
+//public class SelectedGroup extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_selectedgroup);
+//
+//        Intent selectedgroup = getIntent();
+//        String groupName = selectedgroup.getStringExtra("groupName");
+//        String groupCode = selectedgroup.getStringExtra("groupCode");
+//
+//        TextView setGroupName = findViewById(R.id.groupname);
+//        TextView setGroupCode = findViewById(R.id.groupcode);
+//
+//        setGroupName.setText(groupName);
+//        setGroupCode.setText(groupCode + " (others can use this code to join your group)");
+//
+//    }
+//}
+
+public class SelectedGroup extends Fragment implements View.OnClickListener {
+
+    private static final String TAG = "SelectedGroup";
+    String groupName, groupCode;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_selectedgroup);
+    public void onAttach(@NonNull Context context){
+        super.onAttach(context);
+        Bundle args = getArguments();
+        groupName = args.getString("groupName");
+        groupCode = args.getString("groupCode");
+    }
 
-        Intent selectedgroup = getIntent();
-        String groupName = selectedgroup.getStringExtra("groupName");
-        String groupCode = selectedgroup.getStringExtra("groupCode");
+    public SelectedGroup(){
+        //Required empty public constructor
+    }
 
-        TextView setGroupName = findViewById(R.id.groupname);
-        TextView setGroupCode = findViewById(R.id.groupcode);
+    @Override
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 
+        View rootView = inflater.inflate(R.layout.activity_selectedgroup, container, false);
+        TextView setGroupName = rootView.findViewById(R.id.groupname);
+        TextView setGroupCode = rootView.findViewById(R.id.groupcode);
         setGroupName.setText(groupName);
         setGroupCode.setText(groupCode + " (others can use this code to join your group)");
+
+        return rootView;
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
