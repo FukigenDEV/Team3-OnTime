f219e42ecc37452c896c0a16e6435e57bc97cafa
laREiGN <43932616+laREiGN@users.noreply.github.com>
2020-06-05 12:58:18 +0200

    quick update to server code  - removed the unneeded newUserNotification function  - Reworked groupJoinedNotification

--- PROTOTYPES/OnTimeApp/functions/index.js
+++ PROTOTYPES/OnTimeApp/functions/index.js
@@ -2,68 +2,78 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+//--------------------------------------------------------------------------------------------//
+// DEPRECATED AS IT IS UNNECCESARY IN THE APP AND WAS ONLY USED FOR TESTING
+
 // SEND NOTIFICATION WHEN DEVICE TOKEN ADDED TO DB
 // Listens for new tokens added to /Users/{AndroidID}/deviceToken and sends
 // notification the device of the new token
-exports.newUserNotification = functions.database.ref('/Users/{AndroidID}/deviceToken')
-    .onWrite((change, context) => {
-        const AndroidID = context.params.AndroidID;
-
-        var ref = admin.database().ref(`/Users/${AndroidID}/deviceToken`);
-        ref.once("value", function(snapshot){
-        const deviceToken = snapshot.val();
-            const payload = {
-                data: {
-                    title: 'Hello from Firebase',
-                    body: 'New user has been added to the database'
-                },
-                token: deviceToken
-            };
-
-            admin.messaging().send(payload)
-            .then(function(response) {
-                return console.log("Successfully sent message:", response);
-                })
-                .catch(function(error) {
-                return console.log("Error sending message:", error);
-                });
-
-        }, function (errorObject){
-            console.log("The read failed: " + errorObject.code);
-        });
-    });
+// exports.newUserNotification = functions.database.ref('/Users/{AndroidID}/deviceToken')
+//     .onWrite((change, context) => {
+//         const AndroidID = context.params.AndroidID;
+//
+//         var ref = admin.database().ref(`/Users/${AndroidID}/deviceToken`);
+//         ref.once("value", function(snapshot){
+//         const deviceToken = snapshot.val();
+//             const payload = {
+//                 data: {
+//                     title: 'Hello from Firebase',
+//                     body: 'New user has been added to the database'
+//                 },
+//                 token: deviceToken
+//             };
+//
+//             admin.messaging().send(payload)
+//             .then(function(response) {
+//                 return console.log("Successfully sent message:", response);
+//                 })
+//                 .catch(function(error) {
+//                 return console.log("Error sending message:", error);
+//                 });
+//
+//         }, function (errorObject){
+//             console.log("The read failed: " + errorObject.code);
+//         });
+//     });
+//--------------------------------------------------------------------------------------------//
 
 // SEND NOTIFICATION WHEN USER ADDED TO
 // Listens for new tokens added to /Groups/{GroupID}/Members/{AndroidID} and sends
 // notification to the members of the groups telling them a new user has joined said group
 exports.groupJoinedNotification = functions.database.ref('/Groups/{GroupID}/Members/{AndroidID}')
     .onWrite((change, context) => {
-        const GroupID = context.params.GroupID;
-        const AndroidID = context.params.AndroidID;
-
-        var ref = admin.database().ref(`/Groups/${GroupID}/Members`);
-        ref.once("value", function(snapshot){
-            snapshot.forEach(function(childSnapshot){
-                const deviceToken = childSnapshot.child("deviceToken").val();
-                const payload = {
-                    data: {
-                        title: 'On Time - New addition to the team!',
-                        body: 'A new user has joined your group in the On Time app. Give them a warm welcome!'
-                    },
-                    token: deviceToken
-                };
-
-                admin.messaging().send(payload)
-                .then(function(response) {
-                    return console.log("Successfully sent message:", response);
-                    })
-                .catch(function(error) {
-                    return console.log("Error sending message:", error);
-                    });
-            });
-        }, function (errorObject){
-            console.log("The read failed: " + errorObject.code);
+      if(change.before.exists()){
+        return null;
+      }
+      if(!change.after.exists()){
+        return null;
+      }
+      const GroupID = context.params.GroupID;
+      const AndroidID = context.params.AndroidID;
+      var ref = admin.database().ref(`/Groups/${GroupID}/Members`);
+      ref.once("value", function(snapshot){
+        snapshot.forEach(function(childSnapshot){
+          const deviceToken = childSnapshot.child("deviceToken").val();
+          if(AndroidID !== deviceToken){
+            const payload = {
+              data: {
+                title:  'On Time - New addition to the team!',
+                body:   'A new user has joined your group in the On Time app. Give them a warm welcome!'
+              },
+              token: deviceToken
+            };
+            admin.messaging().send(payload)
+            .then(function(response) {
+              return console.log("Successfully sent message:", response);
+              })
+              .catch(function(error) {
+              return console.log("Error sending message:", error);
+              });
+          }
         });
+      }, function (errorObject){
+          console.log("The read failed: " + errorObject.code);
+      });
     });
 
 // MONITOR NEW ALARMS
