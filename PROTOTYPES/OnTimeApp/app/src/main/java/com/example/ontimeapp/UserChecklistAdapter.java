15cc90374422c33c16b38da5e9e5e192cb53503e
laREiGN <43932616+laREiGN@users.noreply.github.com>
2020-06-08 00:40:45 +0200

    first changes

---
+++ PROTOTYPES/OnTimeApp/app/src/main/java/com/example/ontimeapp/UserChecklistAdapter.java
@@ -0,0 +1,17 @@
+package com.example.ontimeapp;
+
+import android.content.Context;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class UserChecklistAdapter extends RecyclerView.Adapter<UserChecklistAdapter.MyViewHolder> {
+
+    ArrayList<String> Tasks, Status;
+    Context;
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        
+    }
+}
